72b4665af8351871d39e9283342b0ef045c55ee1
Andrewtheprince <andry.mazzo@outlook.com>
2025-03-17 11:12:48 +0100

    creato app, controller e view

---
+++ controller.py
@@ -0,0 +1,8 @@
+from view import View
+
+class Controller:
+    def __init__(self, v: View):
+        self._view = v
+
+
+
---
+++ view.py
@@ -0,0 +1,14 @@
+import flet as ft
+
+class View:
+    def __init__(self, page: ft.Page):
+        self._controller = None
+        self._page = page
+
+
+    def loadInterface(self):
+
+
+    def setController(self, c):
+        self._controller = c
+
