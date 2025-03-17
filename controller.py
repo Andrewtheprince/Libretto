fc12997ac08a2e2fea72317dd2520cd2f6002bbd
Andrewtheprince <andry.mazzo@outlook.com>
2025-03-17 11:24:51 +0100

    creato app, controller e view

--- controller.py
+++ controller.py
@@ -4,5 +4,13 @@ class Controller:
     def __init__(self, v: View):
         self._view = v
 
-
+    def handleAggiungi(self, e):
+        strIn = self._view._txtIn.value
+        if strIn =="":
+            self._view._txtOut.value = "Errore: campo vuoto"
+            self._view._page.update()
+            return
+        else:
+            self._view._txtOut.value=strIn
+            self._view._page.update()
 
--- view.py
+++ view.py
@@ -2,12 +2,21 @@ import flet as ft
 
 class View:
     def __init__(self, page: ft.Page):
+        self._txtOut = None
+        self._btnIn = None
+        self._txtIn = None
         self._controller = None
         self._page = page
 
 
     def loadInterface(self):
-        pass
+        
+        self._txtIn = ft.TextField(label ="Inserisci nome")
+        self._btnIn = ft.ElevatedButton("Aggiungi", on_click=self._controller.handleAggiungi)
+        row = ft.Row(controls = [self._txtIn, self._btnIn])
+        self._txtOut = ft.Text("")
+        
+        self._page.add(row, self._txtOut)
 
     def setController(self, c):
         self._controller = c
